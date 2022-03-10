d4842432b55b37fd2e0be0c47e64401ab34be3a8
Sydney Wilson <syd.toon17@gmail.com>
2022-03-10 15:08:00 -0600

    created song seirizer class and file

---
+++ songs_lib/models.py
@@ -0,0 +1,10 @@
+from platform import release
+from django.db import models
+
+# Create your models here.
+class Songs(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateField()
+    title = models.CharField(max_length=255)
---
+++ songs_lib/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers;
+from .models import Songs;
+
+class SongsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Songs
+        fields = ['id', 'title', 'artist', 'album', 'release_date']
