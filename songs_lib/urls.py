b4fb1aff836f03803ca493124a77f7a3e0e3f42b
Sydney Wilson <syd.toon17@gmail.com>
2022-03-11 13:28:58 -0600

    Completed GET, PUT and DELETE fuctionality for app

--- songs_lib/urls.py
+++ songs_lib/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.songs_list),
-    # path('<int:pk>/', views.music),
+    path('<int:pk>/', views.music),
 ]
--- songs_lib/views.py
+++ songs_lib/views.py
@@ -18,3 +18,18 @@ def songs_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def music(request, pk):
+    song = get_object_or_404(Songs, pk=pk)
+    if request.method == 'GET':
+        serializer = SongsSerializer(song);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongsSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
