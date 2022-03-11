989d216ae26eec6b1f6394a1ec5705c7e84cc6ee
Sydney Wilson <syd.toon17@gmail.com>
2022-03-11 11:30:06 -0600

    Completed GET command + fixed urls

--- songs_lib/urls.py
+++ songs_lib/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('music/', views.songs_list),
-    path('<int:pk>/', views.music),
+    path('', views.songs_list),
+    # path('<int:pk>/', views.music),
 ]
--- songs_lib/views.py
+++ songs_lib/views.py
@@ -1,7 +1,20 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Songs
+from .serializers import SongsSerializer
+from rest_framework import status
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def songs_list(request):
-    return Response('ok')
+    if request.method == 'GET':
+            songs = Songs.objects.all()
+            serializer = SongsSerializer(songs, many=True)
+            return Response(serializer.data)
+        
+    elif request.method == 'POST':
+        serializer = SongsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
